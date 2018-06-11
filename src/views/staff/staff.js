44bae83fc06b3cff26534a53940100c34728db9e
magnus-hansson <magnus.hansson@fritidsresor.se>
2018-06-11 10:09:58 +0200

    added destination option

--- src/views/staff/staff.js
+++ src/views/staff/staff.js
@@ -173,7 +173,7 @@ class Staff extends Component {
             </TabPane>
 
             <TabPane tabId="destination">
-              <Destination />
+              <Destination destination={this.props.destination} />
             </TabPane>
 
             <TabPane tabId="name">
@@ -224,7 +224,8 @@ class Staff extends Component {
 
 function mapStateToProps(state, ownProps) {
   return {
-    headOf: state.staff.active.headOf
+    headOf: state.staff.active.headOf,
+    destination: state.staff.active.destination
   }
 }
 
