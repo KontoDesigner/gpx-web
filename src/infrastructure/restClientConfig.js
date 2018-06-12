9b6eaf7453e12970a47251cbdb9791da139d5439
magnus-hansson <magnus.hansson@fritidsresor.se>
2018-06-12 10:02:22 +0200

    typo

--- src/infrastructure/restClientConfig.js
+++ src/infrastructure/restClientConfig.js
@@ -6,12 +6,12 @@ if (hostName === 'localhost') {
   baseUrl = 'http://localhost:5000'
 }
 //UAT
-else if (hostName === 'gpx-uat.tuinordic.net' || hostName === 'gpx.uat.tuinordic.net') {
-  baseUrl = 'https://gpx-uat.tuinordic.net'
+else if (hostName === 'gpx-web-uat.tuinordic.net' || hostName === 'gpx-web.uat.tuinordic.net') {
+  baseUrl = 'https://gpx-web-uat.tuinordic.net'
 }
 //PROD
-else if (hostName === 'gpx.tuinordic.net') {
-  baseUrl = 'https://gpx.tuinordic.net'
+else if (hostName === 'gpx-web.tuinordic.net') {
+  baseUrl = 'https://gpx-web.tuinordic.net'
 } else {
   console.warn(`could not identify hostname: ${hostName}.`)
 }
