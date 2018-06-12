607f28796e9896aa29cc7a5b91ffb975ed6e9a6d
magnus-hansson <magnus.hansson@fritidsresor.se>
2018-06-12 10:11:05 +0200

    wtf

--- src/infrastructure/restClientConfig.js
+++ src/infrastructure/restClientConfig.js
@@ -6,12 +6,12 @@ if (hostName === 'localhost') {
   baseUrl = 'http://localhost:5000'
 }
 //UAT
-else if (hostName === 'gpx-web-uat.tuinordic.net' || hostName === 'gpx-web.uat.tuinordic.net') {
-  baseUrl = 'https://gpx-web-uat.tuinordic.net'
+else if (hostName === 'gpx-api-uat.tuinordic.net' || hostName === 'gpx-api.uat.tuinordic.net') {
+  baseUrl = 'https://gpx-api-uat.tuinordic.net'
 }
 //PROD
 else if (hostName === 'gpx-web.tuinordic.net') {
-  baseUrl = 'https://gpx-web.tuinordic.net'
+  baseUrl = 'https://gpx-api.tuinordic.net'
 } else {
   console.warn(`could not identify hostname: ${hostName}.`)
 }
