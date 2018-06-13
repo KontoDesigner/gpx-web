2b6c729d5c6cb23bb7b78bbbb54265dc54be4ae6
Filip Danielsson <filip.danielsson@tui.se>
2018-06-13 19:23:50 +0200

    removed tabs and views

--- src/views/staff/staff.js
+++ src/views/staff/staff.js
@@ -3,18 +3,12 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { TabContent, TabPane, Card, Row, Col, CardBody, CardHeader, ListGroup, ListGroupItem } from 'reactstrap'
 import classnames from 'classnames'
-import A1 from './active/a1/a1'
-import DepartureArrival from './active/departureArrival/departureArrival'
 import Destination from './active/destination/destination'
 import HeadOf from './active/headOf/headOf'
 import JobTitle from './active/jobTitle/jobTitle'
 import Name from './active/name/name'
-import Archive from './inactive/archive/archive'
 import RecentlyInactive from './inactive/recentlyInactive/recentlyInactive'
-import ITSDAdmin from './other/itsdAdmin/itsdAdmin'
 import NewEmployees from './other/newEmployees/newEmployees'
-import SaveConflicts from './other/saveConflicts/saveConflicts'
-import TUIProfileLogin from './other/tuiProfileLogin/tuiProfileLogin'
 import * as headOfActions from '../../actions/staff/active/headOfActions'
 import * as destinationActions from '../../actions/staff/active/destinationActions'
 import * as filterActions from '../../actions/staff/filterActions'
@@ -35,7 +29,7 @@ class Staff extends Component {
     this.props.filterActions.getSourceMarkets()
   }
 
-  resetData = () => {}
+  resetData = () => { }
 
   toggle = (tab, getData, resetData) => {
     if (this.state.activeTab !== tab) {
@@ -94,24 +88,6 @@ class Staff extends Component {
                       }}>
                       Job Title
                     </ListGroupItem>
-                    <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'departureArrival' })}
-                      onClick={() => {
-                        this.toggle(
-                          'departureArrival',
-                          this.props.departureArrivalActions.getDepartureArrival,
-                          this.props.departureArrivalActions.handleDepartureArrival
-                        )
-                      }}>
-                      Departure & Arrival
-                    </ListGroupItem>
-                    <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'a1' })}
-                      onClick={() => {
-                        this.toggle('a1', this.props.a1Actions.getA1, this.props.a1Actions.handleA1)
-                      }}>
-                      A1
-                    </ListGroupItem>
                   </ListGroup>
                 </CardBody>
               </Card>
@@ -132,13 +108,6 @@ class Staff extends Component {
                       }}>
                       Recently Inactive
                     </ListGroupItem>
-                    <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'archive' })}
-                      onClick={() => {
-                        this.toggle('archive', this.props.archiveActions.getArchive, this.props.archiveActions.handleArchive)
-                      }}>
-                      Archive (2016-10)
-                    </ListGroupItem>
                   </ListGroup>
                 </CardBody>
               </Card>
@@ -149,40 +118,11 @@ class Staff extends Component {
                 <CardBody className="no-padding">
                   <ListGroup>
                     <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'itsdAdmin' })}
-                      onClick={() => {
-                        this.toggle('itsdAdmin', this.props.itsdAdminActions.getItsdAdmin, this.props.itsdAdminActions.handleItsdAdmin)
-                      }}>
-                      ITSD Admin
-                    </ListGroupItem>
-                    <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'saveConflicts' })}
-                      onClick={() => {
-                        this.toggle(
-                          'saveConflicts',
-                          this.props.saveConflictsActions.getSaveConflicts,
-                          this.props.saveConflictsActions.handleSaveConflicts
-                        )
-                      }}>
-                      Save Conflicts
-                    </ListGroupItem>
-                    <ListGroupItem
                       className={classnames({ active: this.state.activeTab === 'newEmployees' })}
                       onClick={() => {
                         this.toggle('newEmployees', this.props.newEmployeesActions.getNewEmployees, this.props.newEmployeesActions.handleNewEmployees)
                       }}>
                       New Employees
-                    </ListGroupItem>
-                    <ListGroupItem
-                      className={classnames({ active: this.state.activeTab === 'tuiProfileLogin' })}
-                      onClick={() => {
-                        this.toggle(
-                          'tuiProfileLogin',
-                          this.props.tuiProfileLoginActions.getTuiProfileLogin,
-                          this.props.tuiProfileLoginActions.handleTuiProfileLogin
-                        )
-                      }}>
-                      TUI Profile Login
                     </ListGroupItem>
                   </ListGroup>
                 </CardBody>
@@ -213,51 +153,15 @@ class Staff extends Component {
               />
             </TabPane>
 
-            <TabPane tabId="departureArrival">
-              <DepartureArrival
-              //getDepartureArrival={this.props.departureArrivalActions.getDepartureArrival}
-              />
-            </TabPane>
-
-            <TabPane tabId="a1">
-              <A1
-              //getA1={this.props.a1Actions.getA1}
-              />
-            </TabPane>
-
             <TabPane tabId="recentlyInactive">
               <RecentlyInactive
               //getRecentlyInactive={this.props.recentlyInactiveActions.getRecentlyInactive}
               />
             </TabPane>
 
-            <TabPane tabId="archive">
-              <Archive
-              //getArchive={this.props.archiveActions.getArchive}
-              />
-            </TabPane>
-
-            <TabPane tabId="itsdAdmin">
-              <ITSDAdmin
-              //getItsdAdmin={this.props.itsdAdminActions.getItsdAdmin}
-              />
-            </TabPane>
-
-            <TabPane tabId="saveConflicts">
-              <SaveConflicts
-              //getSaveConflicts={this.props.saveConflictsActions.getSaveConflicts}
-              />
-            </TabPane>
-
             <TabPane tabId="newEmployees">
               <NewEmployees
               //getNewEmployees={this.props.newEmployeesActions.getNewEmployees}
-              />
-            </TabPane>
-
-            <TabPane tabId="tuiProfileLogin">
-              <TUIProfileLogin
-              //getTuiProfileLogin={this.props.tuiProfileLoginActions.getTuiProfileLogin}
               />
             </TabPane>
           </TabContent>
