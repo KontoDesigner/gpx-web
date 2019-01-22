b838648f84539396935a0db66d135f6ff41b3c99
Lasse Lindén <Lars.Linden@fritidsresor.se>
2019-01-22 10:38:40 +0100

    import style

--- src/views/staff/other/imports/import.js
+++ src/views/staff/other/imports/import.js
@@ -1,54 +1,74 @@
-
 import React, { Component } from 'react'
-import Moment from 'react-moment';
-import { Row, Col,  Label, Input,Button ,Card,CardHeader,CardBody} from 'reactstrap'
+import Moment from 'react-moment'
+import {
+  Row,
+  Col,
+  Label,
+  Input,
+  Button,
+  Card,
+  CardHeader,
+  CardBody
+} from 'reactstrap'
 import TextInput from '../../../../components/textInput'
 import Select from 'react-select'
 
 import Datetime from 'react-datetime'
 
+const Import = props => {
+  return (
+    <Card>
+      <CardHeader>File Imports</CardHeader>
 
-const Import = (props) => {
-    return (
- 
-        <Card>
-            <CardHeader>File Imports</CardHeader>
+      <CardBody className="no-padding-bottom">
+        <div className="form-row">
+          <Col
+            key={0}
+            sm="12"
+            md="4"
+            lg="3"
+            xl="3"
+            className="form-group form-group-select"
+          >
+            <label htmlFor="importTypes">Select File Import Typeww</label>
 
-            <CardBody className="no-padding-bottom">
-                <div className="form-row">
-
-        <Col key={0} sm="12" md="4" lg="3" xl="3" className="form-group form-group-select">
-
- <label htmlFor="importTypes">Select File Import Typeww</label>
-
-        <Select 
-          id="importTypes"
-          valueKey="name"
-          labelKey="id"
-          className="form-control form-group-select"
-          options={props.importTypes}
-       onChange = {props.handleImportType }
-          value={props.importType}
-         placeholder="Select"
-        />
-
-</Col></div>
+            <Select
+              id="importTypes"
+              valueKey="name"
+              labelKey="id"
+              className="form-control form-group-select"
+              options={props.importTypes}
+              onChange={props.handleImportType}
+              value={props.importType}
+              placeholder="Select"
+            />
+          </Col>
+        </div>
         <div className="row">
           <Col key={0} sm="12" md="4" lg="3" xl="3" className="form-group ">
-<input
-onChange={e => props.handleFile(e.target.files[0])}
- type="file"
-name="file" 
-id="file"
-/>
-       
-           <Button   color="success"  onClick={() => { props.create() }}>Start Import</Button>
-  
-      </Col>
+            <input
+              onChange={e => props.handleFile(e.target.files[0])}
+              type="file"
+              name="file"
+              id="file"
+            />
+</Col>
 </div>
-            </CardBody>
-        </Card>
-    );
-};
+<div className="row">
+<Col key={0} sm="12" md="4" lg="3" xl="3" className="form-group ">
+            <Button
+              color="success"
+              onClick={() => {
+                props.create()
+              }}
+            >
+              Start Import
+            </Button>
+          </Col>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
 
 export default Import
