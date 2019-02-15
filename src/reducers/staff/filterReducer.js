fb0658f40348bf23e97b6dd8c83eec52da4b56ee
Filip Danielsson <filip.danielsson@tui.se>
2019-02-15 09:49:38 +0100

    filter text local state uglify

--- src/reducers/staff/filterReducer.js
+++ src/reducers/staff/filterReducer.js
@@ -1,38 +1,37 @@
 import { ActionTypes as types } from '../../constants/staff/filterConstants'
 
 var defaultState = {
-  text: '',
-  sourceMarket: null,
-  selectedJobFamily: null,
-  selectedStaff: []
+    text: '',
+    sourceMarket: null,
+    selectedJobFamily: null,
+    selectedStaff: []
 }
 
 export default function filterReducer(state = defaultState, action) {
-  switch (action.type) {
-    case types.HANDLE_SELECTEDSTAFF:
-      return {
-        ...state,
-        selectedStaff: action.data.selectedStaff
-      }
-      case types.HANDLE_SELECTEDJOBFAMILY:
-      
-      return {
-        ...state,
-        selectedJobFamily: action.data.selectedJobFamily 
-      }
-    case types.HANDLE_FILTER:
-      return defaultState
-    case types.HANDLE_SOURCEMARKET:
-      return {
-        ...state,
-        sourceMarket: action.data.sourceMarket
-      }
-    case types.HANDLE_TEXT:
-      return {
-        ...state,
-        text: action.data.text
-      }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case types.HANDLE_SELECTEDSTAFF:
+            return {
+                ...state,
+                selectedStaff: action.data.selectedStaff
+            }
+        case types.HANDLE_SELECTEDJOBFAMILY:
+            return {
+                ...state,
+                selectedJobFamily: action.data.selectedJobFamily
+            }
+        case types.HANDLE_FILTER:
+            return defaultState
+        case types.HANDLE_SOURCEMARKET:
+            return {
+                ...state,
+                sourceMarket: action.data.sourceMarket
+            }
+        case types.HANDLE_TEXT:
+            return {
+                ...state,
+                text: action.data.text
+            }
+        default:
+            return state
+    }
 }
