8c81f73b1f8ca4931ebbb40af191d41e8a220aae
Lasse Lindén <Lars.Linden@fritidsresor.se>
2019-04-05 13:30:17 +0200

    SendMail

--- src/views/staff/sendMailUsingTemplate.js
+++ src/views/staff/sendMailUsingTemplate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import AutoWords from '../../components/autoWords'
 import {
   Button,
   Modal,
@@ -215,51 +216,8 @@ debugger;
   {this.state.selectedNotification !== null ?
                             <Row>
                                 <Col>  
-                                <UncontrolledDropdown>
-      <DropdownToggle caret>
-       Insert Auto-Word
-      </DropdownToggle>
-      <DropdownMenu>
-
-      <DropdownItem title="firstName" onClick={() => {this.p.getSelection('<FIRSTNAME>')}}>
-               FirstName  </DropdownItem>
-       
-               <DropdownItem title="firstName" onClick={() => {this.getSelection('<LASTNAME>')}}>
-               LastName  </DropdownItem>
-       
-               <DropdownItem title="firstName" onClick={() => {this.getSelection('<FORMNAME>')}}>
-               FormName  </DropdownItem>
-
-                  <DropdownItem title="firstName" onClick={() => {this.getSelection('<NEXTDESTINATION>')}}>
-               NextDestination  </DropdownItem>
-
-                  <DropdownItem title="firstName" onClick={() => {this.getSelection('<NEXTPOSITION>')}}>
-               NextPosition </DropdownItem>
-
-                  <DropdownItem title="firstName" onClick={() => {this.getSelection('<PLACEMENTDATE>')}}>
-               Placement Date </DropdownItem>
-
-                  <DropdownItem title="firstName" onClick={() => {this.getSelection('<LASTWORKDATE>')}}>
-               Last Work Date </DropdownItem>
-
-                 <DropdownItem title="firstName" onClick={() => {this.getSelection('<CENTRALID>')}}>
-               CentralID </DropdownItem>
-
-                 <DropdownItem title="firstName" onClick={() => {this.getSelection('<NATIONALITY>')}}>
-               Nationality </DropdownItem>
-
-                <DropdownItem title="firstName" onClick={() => {this.getSelection('<CURRENTPOSITION>')}}>
-               Current Position</DropdownItem>
-
-                 <DropdownItem title="firstName" onClick={() => {this.getSelection('<CURRENTDESTINATION>')}}>
-               Current Destination</DropdownItem>
-
-               {/* <DropdownItem title="undo" onClick={() => {this.props.undoSelection('<UNDO>')}}>
-               Undo Last Selection</DropdownItem> */}
-      
-      </DropdownMenu>
-
-    </UncontrolledDropdown>
+                                <AutoWords />
+                                
   
                                 <Input
                         required
