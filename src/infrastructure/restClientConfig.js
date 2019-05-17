e6985d61f6503e1e6f020a088366fe0d60af2e54
Filip Danielsson <filip.danielsson@tui.se>
2019-05-17 10:35:45 +0200

    dev

--- src/infrastructure/restClientConfig.js
+++ src/infrastructure/restClientConfig.js
@@ -3,22 +3,26 @@ const hostName = window.location.hostname
 
 //LOCAL
 if (hostName === 'localhost') {
-  baseUrl = 'http://localhost:5000'
- // baseUrl = 'https://gpx-api-uat.tuinordic.net'
+    baseUrl = 'http://localhost:5000'
+    // baseUrl = 'https://gpx-api-uat.tuinordic.net'
+}
+//
+else if (hostName === 'gpx-web.dev.tuinordic.com') {
+    baseUrl = 'http://gpx-api.dev.tuinordic.com'
 }
 //UAT
 else if (hostName === 'gpx-web-uat.tuinordic.net' || hostName === 'gpx-web.uat.tuinordic.net') {
-  baseUrl = 'https://gpx-api-uat.tuinordic.net'
+    baseUrl = 'https://gpx-api-uat.tuinordic.net'
 }
 //PROD
 else if (hostName === 'gpx-web.tuinordic.net') {
-  baseUrl = 'https://gpx-api.tuinordic.net'
+    baseUrl = 'https://gpx-api.tuinordic.net'
 } else {
-  console.warn(`could not identify hostname: ${hostName}.`)
+    console.warn(`could not identify hostname: ${hostName}.`)
 }
 
 export const restClientConfig = {
-  baseUrl: baseUrl
+    baseUrl: baseUrl
 }
 
 export default restClientConfig
