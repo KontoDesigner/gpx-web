2b072f0ecd7214c79e6466ce68ee5e340c317241
Filip Danielsson <filip.danielsson@tui.se>
2019-09-24 14:55:33 +0200

    aws conf

--- src/infrastructure/restClientConfig.js
+++ src/infrastructure/restClientConfig.js
@@ -6,7 +6,7 @@ if (hostName === 'localhost') {
     baseUrl = 'http://localhost:5001'
     // baseUrl = 'https://gpx-api-uat.tuinordic.net'
 }
-//
+//DEV
 else if (hostName === 'gpx-web.dev.tuinordic.com') {
     baseUrl = 'http://gpx-api.dev.tuinordic.com'
 }
@@ -18,9 +18,17 @@ else if (hostName === 'gpx-web-uat.tuinordic.net' || hostName === 'gpx-web.uat.t
 else if (hostName === 'gpx-web.tuinordic.net') {
     baseUrl = 'https://gpx-api.tuinordic.net'
 }
+//DEV
+else if (hostName === 'gpx-web.test.tui-dx.com') {
+    baseUrl = 'http://gpx-api.test.tui-dx.com'
+}
+//UAT
+else if (hostName === 'gpx-web.pre.tui-dx.com') {
+    baseUrl = 'http://gpx-api.pre.tui-dx.com'
+}
 //PROD
-else if (hostName === 'gpx-web.tpp-project.local') {
-    baseUrl = 'https://gpx-api.tpp-project.local'
+else if (hostName === 'gpx-web.tui-dx.com') {
+    baseUrl = 'https://gpx-api.tui-dx.com'
 } else {
     console.warn(`could not identify hostname: ${hostName}.`)
 }
